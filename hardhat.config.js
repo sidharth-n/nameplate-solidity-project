f09a6b72cfd259c485fa90d067413a8c59c29a90
sidharth-n <sidharthnofficial@gmail.com>
2024-09-02 13:34:37 +0530

    Connect to MetaMask done

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
-};
+  networks: {
+    sonicTestNet: {
+      url: "https://rpc.sonic.fantom.network/",
+      chainId: 64165,
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: "auto",
+    },
+  },
+}
